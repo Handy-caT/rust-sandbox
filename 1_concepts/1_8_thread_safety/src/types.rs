3ad0df8418229fd1f04bc71289000bba7559eca2
Maxim Volkov <maksim.volkov.03@gmail.com>
2023-08-24 12:05:37 +0200

    little changes

--- 1_concepts/1_8_thread_safety/src/types.rs
+++ 1_concepts/1_8_thread_safety/src/types.rs
@@ -1,5 +1,6 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::sync::MutexGuard;
 
 struct OnlySend<T>
 where T: Send {
@@ -26,23 +27,32 @@ struct SyncAndSend {
     value: u32
 }
 
+
+#[derive(Debug)]
+struct SyncAndNotSend {
+    value: u32,
+    phantom: std::marker::PhantomData<MutexGuard<'static, u32>>
+}
+
 #[cfg(test)]
 mod tests {
     use std::cell::RefCell;
-    use crate::types::{OnlySend, SyncAndSend};
+    use std::sync::Arc;
+    use std::thread;
+    use crate::types::{OnlySend, SyncAndNotSend, SyncAndSend};
 
     #[test]
     fn test_sync_and_send() {
         let sync_and_send = SyncAndSend { value: 1 };
 
-        let handle = std::thread::spawn(move || {
+        let handle = thread::spawn(move || {
             println!("sync_and_send send: {}", sync_and_send.value);
         });
         handle.join().unwrap();
 
-        let reference = Box::new(sync_and_send);
+        let reference = Arc::new(sync_and_send);
 
-        let handle = std::thread::spawn(move || {
+        let handle = thread::spawn(move || {
             println!("sync_and_send sync: {}", reference.value);
         });
         handle.join().unwrap();
@@ -52,7 +62,7 @@ mod tests {
     fn test_only_send() {
         let only_send = OnlySend { value: RefCell::new(1) };
 
-        let handle = std::thread::spawn(move || {
+        let handle = thread::spawn(move || {
             println!("only_send send: {:?}", only_send.value);
         });
         handle.join().unwrap();
