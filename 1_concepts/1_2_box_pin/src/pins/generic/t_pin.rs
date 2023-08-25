6a54fb01efddab88334d3c348b603fa4d6e40a44
Maxim Volkov <maksim.volkov.03@gmail.com>
2023-08-25 11:56:28 +0200

    updated t_pin

--- 1_concepts/1_2_box_pin/src/pins/generic/t_pin.rs
+++ 1_concepts/1_2_box_pin/src/pins/generic/t_pin.rs
@@ -2,13 +2,17 @@ use std::fmt::Debug;
 use std::future::Future;
 use std::pin::Pin;
 use crate::pins::base_traits::{MutMeSomehow, SayHi};
+use futures::task::noop_waker;
 
 impl<T> SayHi for T where T: Debug {
 }
 
 impl<T> MutMeSomehow for T where T: Debug + Default + Future {
     fn mut_me_somehow(self: Pin<&mut Self>) {
+        let waker = noop_waker();
+        let mut cx = std::task::Context::from_waker(&waker);
 
+        self.poll(&mut cx);
     }
 }
 
