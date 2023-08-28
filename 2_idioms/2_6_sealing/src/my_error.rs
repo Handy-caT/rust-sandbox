2a1eaa67ba12dbf2615821b57abcab917eec1c14
Maxim Volkov <maksim.volkov.03@gmail.com>
2023-08-28 12:27:18 +0200

    added 2_6 solution

--- 2_idioms/2_6_sealing/src/my_error.rs
+++ 2_idioms/2_6_sealing/src/my_error.rs
@@ -6,7 +6,30 @@ use std::{
     fmt::{Debug, Display},
 };
 
+mod private {
+    pub struct Internal;
+}
+
 /// Basic expectations for error values.
+///```compile_fail
+/// use std::any::TypeId;
+/// use std::fmt::{Debug, Display, Formatter};
+/// use step_2_6::MyError;
+///
+/// #[derive(Debug)]
+/// struct TestError;
+///
+/// impl Display for TestError {
+///     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+///         write!(f, "TestError")
+///     }
+/// }
+///
+///impl MyError for TestError {
+///     fn type_id(&self, _: step_2_6::my_error::private::Internal) -> TypeId where Self: 'static {
+///         TypeId::of::<Self>()
+///     }
+/// }
 pub trait MyError: Debug + Display {
     /// The lower-level source of this error, if any.
     ///
@@ -67,7 +90,7 @@ pub trait MyError: Debug + Display {
     ///
     /// __This is memory-unsafe to override in user code.__
     #[doc(hidden)]
-    fn type_id(&self) -> TypeId
+    fn type_id(&self, _: private::Internal) -> TypeId
     where
         Self: 'static,
     {
