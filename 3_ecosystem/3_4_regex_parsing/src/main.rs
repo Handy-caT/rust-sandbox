f0c3b668da23a8350beee60fb4bb97d9c57f323f
Maxim Volkov <maksim.volkov.03@gmail.com>
2023-09-03 13:29:12 +0200

    added second nom parser

--- 3_ecosystem/3_4_regex_parsing/src/main.rs
+++ 3_ecosystem/3_4_regex_parsing/src/main.rs
@@ -1,3 +1,10 @@
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::complete::{alphanumeric1, digit1};
+use nom::combinator::value;
+use nom::Err::Error;
+use nom::IResult;
+use nom::sequence::tuple;
 use regex::Regex;
 
 fn main() {
@@ -12,7 +19,7 @@ trait Parser {
 #[derive(Debug, PartialEq)]
 struct Width(pub Count);
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Sign {
     Plus,
     Minus,
@@ -187,6 +194,190 @@ impl Parser for RegexParser {
     }
 }
 
+struct NomParser;
+
+impl NomParser {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn parse_sign<'a>(&self, input: &'a str) -> IResult<&'a str, Sign> {
+        alt(
+            (
+                value(Sign::Plus, tag("+")),
+                value(Sign::Minus, tag("-")),
+            )
+        )(input)
+    }
+
+    fn parse_align<'a>(&self, input: &'a str) -> IResult<&'a str, (&'a str, &'a str)> {
+        tuple(
+            (
+                alt(
+                    (
+                        alphanumeric1,
+                        tag(""),
+                    )
+                ),
+                alt(
+                    (
+                        tag("<"),
+                        tag(">"),
+                        tag("^"),
+                    )
+                )
+            )
+        )(input)
+    }
+
+    fn parse_grid<'a>(&self, input: &'a str) -> IResult<&'a str, &'a str> {
+        tag("#")(input)
+    }
+
+    fn parse_zero<'a>(&self, input: &'a str) -> IResult<&'a str, &'a str> {
+        tag("0")(input)
+    }
+
+    fn parse_width<'a>(&self, input: &'a str) -> IResult<&'a str, Width> {
+        let res = self.parse_count(input);
+        match res {
+            Ok((input, output)) => {
+                Ok((input, Width(output)))
+            }
+            Err(err) => {
+                Err(err)
+            }
+        }
+    }
+
+    fn parse_precision<'a>(&self, input: &'a str) -> IResult<&'a str, Precision> {
+        let res = tag(".")(input);
+        match res {
+            Ok((input, _)) => {
+                let res = self.parse_count(input);
+                match res {
+                    Ok((input, output)) => {
+                        Ok((input, Precision::Count(output)))
+                    }
+                    Err(err) => {
+                        let res = tag("*")(input);
+                        match res {
+                            Ok((input, _)) => {
+                                Ok((input, Precision::Asterisk))
+                            }
+                            Err(err) => {
+                                Err(err)
+                            }
+                        }
+                    }
+                }
+            }
+            Err(err) => {
+                Err(err)
+            }
+        }
+    }
+
+    fn parse_count<'a>(&self, input: &'a str) -> IResult<&'a str, Count> {
+        let res = self.parse_parameter(input);
+        match res {
+            Ok((input, output)) => {
+                if input.starts_with('$') {
+                    let res = input.strip_prefix('$').unwrap();
+                    Ok((res, Count::Parameter(output)))
+                } else {
+                    let res = digit1(input)?;
+                    let integer = res.1.parse::<usize>().unwrap();
+                    Ok((res.0, Count::Integer(Integer(integer))))
+                }
+            }
+            Err(err) => {
+                let res = digit1(input)?;
+                let integer = res.1.parse::<usize>().unwrap();
+                Ok((res.0, Count::Integer(Integer(integer))))
+            }
+        }
+    }
+
+    fn parse_parameter<'a>(&self, input: &'a str) -> IResult<&'a str, Argument> {
+        let res = self.parse_argument(input);
+        match res {
+            Ok((input, output)) => {
+                if input.starts_with("$") {
+                    Ok((input, output))
+                } else {
+                    Err(Error(nom::error::Error::new(input, nom::error::ErrorKind::Tag)))
+                }
+            }
+            Err(err) => {
+                Err(err)
+            }
+        }
+    }
+
+    fn parse_argument<'a>(&self, input: &'a str) -> IResult<&'a str, Argument> {
+        let res: IResult<&'a str, &'a str, ()> = digit1(input);
+        match res {
+            Ok((input, output)) => {
+                let integer = output.parse::<usize>().unwrap();
+                Ok((input, Argument::Integer(Integer(integer))))
+            }
+            Err(_) => {
+                let res = alphanumeric1(input)?;
+
+                let identifier = res.1.to_string();
+                Ok((res.0, Argument::Identifier(Identifier(identifier))))
+            }
+        }
+    }
+}
+
+impl Parser for NomParser {
+    fn parse(&self, input: &str) -> (Option<Sign>, Option<Width>, Option<Precision>) {
+        let mut input = input;
+        let mut sing = None;
+        let mut width = None;
+        let mut precision = None;
+
+        let align_res = self.parse_align(input);
+        if let Ok(..) = align_res {
+            input = align_res.unwrap().0;
+        }
+
+        let sign_res = self.parse_sign(input);
+        if let Ok(..) = sign_res {
+            let (input_next, output) = sign_res.unwrap();
+            sing = Some(output);
+            input = input_next;
+        }
+
+        let grid_res = self.parse_grid(input);
+        if let Ok(..) = grid_res {
+            input = grid_res.unwrap().0;
+        }
+
+        let zero_res = self.parse_zero(input);
+        if let Ok(..) = zero_res {
+            input = zero_res.unwrap().0;
+        }
+
+        let width_res = self.parse_width(input);
+        if let Ok(..) = width_res {
+            let (input_next, output) = width_res.unwrap();
+            width = Some(output);
+            input = input_next;
+        }
+
+        let precision_res = self.parse_precision(input);
+        if let Ok(..) = precision_res {
+            let (input_next, output) = precision_res.unwrap();
+            precision = Some(output);
+            input = input_next;
+        }
+
+        (sing, width, precision)
+    }
+}
 
 #[cfg(test)]
 mod spec {
@@ -297,21 +488,31 @@ mod spec {
 
     #[test]
     fn parses_sign() {
-        for (input, expected) in vec![
+        let parsers: Vec<Box<dyn Parser>> = vec![
+            Box::new(RegexParser::new()),
+            Box::new(NomParser::new()),
+        ];
+
+        for (input, expected) in [
             ("", None),
             (">8.*", None),
             (">+8.*", Some(Sign::Plus)),
             ("-.1$x", Some(Sign::Minus)),
-            ("a^#043.8?", None),
+            ("a^#043.8?", None)
         ] {
-            let parser = RegexParser::new();
-            let (sign, ..) = parser.parse(input);
-            assert_eq!(sign, expected);
+            for parser in parsers.iter() {
+                let (sign, ..) = parser.parse(input);
+                assert_eq!(sign, expected);
+            }
         }
     }
 
     #[test]
     fn parses_width() {
+        let parsers: Vec<Box<dyn Parser>> = vec![
+            Box::new(RegexParser::new()),
+            Box::new(NomParser::new()),
+        ];
         for (input, expected) in vec![
             ("", None),
             (">8.*", Some(Width(Count::Integer(Integer(8))))),
@@ -321,15 +522,20 @@ mod spec {
             ("-1$.1$x", Some(Width(Count::Parameter(Argument::Integer(Integer(1)))))),
             ("-a1$.1$x", Some(Width(Count::Parameter(Argument::Identifier(Identifier("a1".to_string())))))),
         ] {
-            let parser = RegexParser::new();
-            let (_, width, _) = parser.parse(input);
-            assert_eq!(width, expected);
+            for parser in parsers.iter() {
+                let (_, width, ..) = parser.parse(input);
+                assert_eq!(width, expected);
+            }
         }
     }
 
 
     #[test]
     fn parses_precision() {
+        let parsers: Vec<Box<dyn Parser>> = vec![
+            Box::new(RegexParser::new()),
+            Box::new(NomParser::new()),
+        ];
         for (input, expected) in vec![
             ("", None),
             (">8.*", Some(Precision::Asterisk)),
@@ -339,9 +545,78 @@ mod spec {
             ("-1$.1$x", Some(Precision::Count(Count::Parameter(Argument::Integer(Integer(1)))))),
             ("-1$.a1$x", Some(Precision::Count(Count::Parameter(Argument::Identifier(Identifier("a1".to_string()))))))
         ] {
-            let parser = RegexParser::new();
-            let (_, _, precision) = parser.parse(input);
-            assert_eq!(precision, expected);
+            for parser in parsers.iter() {
+                let (_, _, precision) = parser.parse(input);
+                assert_eq!(precision, expected);
+            }
         }
+    }
+
+    #[test]
+    fn test_parse_sign_nom() {
+        let input = "+";
+        let parser = NomParser::new();
+        let result = parser.parse_sign(input);
+
+        assert_eq!(result.unwrap(), ("", Sign::Plus));
+
+        let input = "-";
+        let result = parser.parse_sign(input);
+
+        assert_eq!(result.unwrap(), ("", Sign::Minus));
+    }
+
+    #[test]
+    fn test_parse_align_nom() {
+        let input = "a^#043.8?";
+        let parser = NomParser::new();
+        let result = parser.parse_align(input);
+
+        assert_eq!(result.unwrap(), ("#043.8?", ("a", "^")));
+    }
+
+    #[test]
+    fn test_parse_grid_nom() {
+        let input = "#043.8?";
+        let parser = NomParser::new();
+        let result = parser.parse_grid(input);
+
+        assert_eq!(result.unwrap(), ("043.8?", "#"));
+    }
+
+    #[test]
+    fn test_parse_nom_full() {
+        let input = "a^+#043.8?";
+        let parser = NomParser::new();
+
+        let result = parser.parse_align(input);
+        assert!(result.is_ok());
+        let (input, align) = result.unwrap();
+        assert_eq!(align, ("a", "^"));
+
+        let result = parser.parse_sign(input);
+        assert!(result.is_ok());
+        let (input, sign) = result.unwrap();
+        assert_eq!(sign, Sign::Plus);
+
+        let result = parser.parse_grid(input);
+        assert!(result.is_ok());
+        let (input, grid) = result.unwrap();
+        assert_eq!(grid, "#");
+
+        let result = parser.parse_zero(input);
+        assert!(result.is_ok());
+        let (input, zero) = result.unwrap();
+        assert_eq!(zero, "0");
+
+        let result = parser.parse_width(input);
+        assert!(result.is_ok());
+        let (input, width) = result.unwrap();
+        assert_eq!(width, Width(Count::Integer(Integer(43))));
+
+        let result = parser.parse_precision(input);
+        assert!(result.is_ok());
+        let (input, precision) = result.unwrap();
+        assert_eq!(precision, Precision::Count(Count::Integer(Integer(8))));
     }
 }
