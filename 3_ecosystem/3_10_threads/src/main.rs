ae853105ddf48cfa6ed1ceb57887f7f829406057
Maxim Volkov <maksim.volkov.03@gmail.com>
2023-09-08 10:29:50 +0200

    first steps

--- 3_ecosystem/3_10_threads/src/main.rs
+++ 3_ecosystem/3_10_threads/src/main.rs
@@ -1,3 +1,33 @@
+use rand::Rng;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+
+fn producer() -> Box<[[u8; 64]; 64]> {
+    let mut rng = rand::thread_rng();
+    let mut result: Box<[[u8; 64]; 64]> = Box::new([[0; 64]; 64]);
+
+    for j in 0..64 {
+        let mut row: [u8; 64] = [0; 64];
+        for i in 0..64 {
+            row[i] = rng.gen();
+        }
+        result[j] = row;
+    };
+
+    result
+}
+
+fn consumer(data: Box<[[u8; 64]; 64]>) {
+    let sum: u32 = data
+        .into_par_iter()
+        .map(|row| row.into_par_iter().map(|x| x as u32).sum::<u32>())
+        .sum();
+
+    println!("Sum: {}", sum);
+}
+
+
 fn main() {
-    println!("Implement me!");
+    let prod = producer();
+    consumer(prod);
+
 }
