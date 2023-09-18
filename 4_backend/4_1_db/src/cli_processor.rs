52d52815171b70d91b31dc8f88e6102e03424a7d
Maxim Volkov <maksim.volkov.03@gmail.com>
2023-09-18 11:26:26 +0300

    updated and finalised 4_1

--- 4_backend/4_1_db/src/cli_processor.rs
+++ 4_backend/4_1_db/src/cli_processor.rs
@@ -6,12 +6,25 @@ pub struct CliProcessor {
     matches: ArgMatches
 }
 
+pub struct UserId(pub i32);
+pub struct RoleSlug(pub String);
+pub struct UserName(pub String);
+pub struct RoleName(pub String);
+pub struct RolePermissions(pub String);
+
+
 pub enum CliCommand {
     AddUser(user::ActiveModel, users_roles::ActiveModel),
     AddRole(role::ActiveModel),
-    DeleteUserName(String),
-    DeleteUserId(i32),
-    DeleteRoleSlug(String),
+    DeleteUserId(UserId),
+    DeleteRoleSlug(RoleSlug),
+    UpdateUser(UserId, UserName),
+    UpdateRole(RoleSlug, Option<RoleName>, Option<RolePermissions>),
+    AssignRole(UserId, RoleSlug),
+    ShowUsers,
+    ShowRoles,
+    ShowUser(UserId),
+    ShowRole(RoleSlug),
 }
 
 impl CliProcessor {
@@ -60,21 +73,64 @@ impl CliProcessor {
     fn process_delete(matches: &ArgMatches) -> CliCommand {
         match matches.subcommand() {
             Some(("user", sub_matches)) => {
-                let name = sub_matches.get_one::<String>("name");
                 let id = sub_matches.get_one::<i32>("id");
 
-                if name.is_some() {
-                    CliCommand::DeleteUserName(name.unwrap().to_owned())
-                } else if id.is_some() {
-                    CliCommand::DeleteUserId(id.unwrap().to_owned())
-                } else {
-                    unreachable!()
-                }
+                CliCommand::DeleteUserId(UserId(id.unwrap().to_owned()))
             }
             Some(("role", sub_matches)) => {
                 let slug = sub_matches.get_one::<String>("slug");
 
-                CliCommand::DeleteRoleSlug(slug.unwrap().to_owned())
+                CliCommand::DeleteRoleSlug(RoleSlug(slug.unwrap().to_owned()))
+            }
+            _ => unreachable!()
+        }
+    }
+
+    fn process_update(matches: &ArgMatches) -> CliCommand {
+        match matches.subcommand() {
+            Some(("user", sub_matches)) => {
+                let id = sub_matches.get_one::<i32>("id");
+                let name = sub_matches.get_one::<String>("name");
+
+                CliCommand::UpdateUser(UserId(id.unwrap().to_owned()), UserName(name.unwrap().to_owned()))
+            }
+            Some(("role", sub_matches)) => {
+                let slug = sub_matches.get_one::<String>("slug");
+                let name = sub_matches.get_one::<String>("name");
+                let permissions = sub_matches.get_one::<String>("permissions");
+
+                CliCommand::UpdateRole(RoleSlug(slug.unwrap().to_owned()),
+                                       name.map(|name| RoleName(name.to_owned())),
+                                       permissions.map(|permissions| RolePermissions(permissions.to_owned())))
+            }
+            _ => unreachable!()
+        }
+    }
+
+    fn process_assign(matches: &ArgMatches) -> CliCommand {
+        let id = matches.get_one::<i32>("user_id");
+        let slug = matches.get_one::<String>("role_slug");
+
+        CliCommand::AssignRole(UserId(id.unwrap().to_owned()), RoleSlug(slug.unwrap().to_owned()))
+    }
+
+    fn process_show(matches: &ArgMatches) -> CliCommand {
+        match matches.subcommand() {
+            Some(("user", sub_matches)) => {
+                let id = sub_matches.get_one::<i32>("id");
+                if id.is_none() {
+                    return CliCommand::ShowUsers;
+                }
+
+                CliCommand::ShowUser(UserId(id.unwrap().to_owned()))
+            }
+            Some(("role", sub_matches)) => {
+                let slug = sub_matches.get_one::<String>("slug");
+                if slug.is_none() {
+                    return CliCommand::ShowRoles;
+                }
+
+                CliCommand::ShowRole(RoleSlug(slug.unwrap().to_owned()))
             }
             _ => unreachable!()
         }
@@ -87,6 +143,15 @@ impl CliProcessor {
             }
             Some(("delete", sub_matches)) => {
                 CliProcessor::process_delete(sub_matches)
+            }
+            Some(("update", sub_matches)) => {
+                CliProcessor::process_update(sub_matches)
+            }
+            Some(("assign", sub_matches)) => {
+                CliProcessor::process_assign(sub_matches)
+            }
+            Some(("show", sub_matches)) => {
+                CliProcessor::process_show(sub_matches)
             }
             _ => unreachable!()
         }
--- 4_backend/4_1_db/src/db_processor.rs
+++ 4_backend/4_1_db/src/db_processor.rs
@@ -1,7 +1,16 @@
-use sea_orm::{ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, ModelTrait, QueryFilter};
+use sea_orm::{ActiveModelTrait, ColumnTrait, ConnectionTrait, DatabaseConnection, DbBackend, EntityTrait, FromQueryResult, JoinType, ModelTrait, QueryFilter, QuerySelect, QueryTrait, RelationDef, Statement};
 use sea_orm::ActiveValue::Set;
-use entities::user;
+use entities::{user, users_roles};
+use entities::user::Model;
 use crate::cli_processor::CliCommand;
+
+
+#[derive(FromQueryResult, Debug)]
+struct UserAndRole {
+    id: i32,
+    name: String,
+    role_slug: String,
+}
 
 pub struct DBProcessor {
     db: DatabaseConnection
@@ -24,17 +33,8 @@ impl DBProcessor {
             CliCommand::AddRole(role) => {
                 role.insert(&self.db).await.unwrap();
             }
-            CliCommand::DeleteUserName(name) => {
-                let user = user::Entity::find()
-                    .filter(user::Column::Name.contains(name))
-                    .one(&self.db)
-                    .await
-                    .unwrap()
-                    .unwrap();
-                user.delete(&self.db).await.unwrap();
-            }
             CliCommand::DeleteUserId(id) => {
-                let user = user::Entity::find_by_id(id)
+                let user = user::Entity::find_by_id(id.0)
                     .one(&self.db)
                     .await
                     .unwrap()
@@ -43,12 +43,117 @@ impl DBProcessor {
             }
             CliCommand::DeleteRoleSlug(slug) => {
                 let role = entities::role::Entity::find()
-                    .filter(entities::role::Column::Slug.contains(slug))
+                    .filter(entities::role::Column::Slug.contains(slug.0))
                     .one(&self.db)
                     .await
                     .unwrap()
                     .unwrap();
                 role.delete(&self.db).await.unwrap();
+            }
+            CliCommand::UpdateUser(id, name) => {
+                let user = user::Entity::find_by_id(id.0)
+                    .one(&self.db)
+                    .await
+                    .unwrap();
+
+                let mut user: user::ActiveModel = user.unwrap().into();
+                user.name = Set(name.0);
+
+                user.update(&self.db).await.unwrap();
+            }
+            CliCommand::UpdateRole(slug, name, permissions) => {
+                let role = entities::role::Entity::find()
+                    .filter(entities::role::Column::Slug.eq(slug.0))
+                    .one(&self.db)
+                    .await
+                    .unwrap();
+
+                let mut role: entities::role::ActiveModel = role.unwrap().into();
+
+                if let Some(name) = name {
+                    role.name = Set(name.0);
+                }
+
+                if let Some(permissions) = permissions {
+                    role.permissions = Set(permissions.0);
+                }
+
+                role.update(&self.db).await.unwrap();
+            }
+            CliCommand::AssignRole(user_id, role_slug) => {
+                let user_role = users_roles::Entity::find()
+                    .filter(users_roles::Column::UserId.eq(user_id.0))
+                    .filter(users_roles::Column::RoleSlug.eq(&role_slug.0))
+                    .one(&self.db)
+                    .await
+                    .unwrap();
+
+                if user_role.is_none() {
+                    let user_role = users_roles::ActiveModel {
+                        user_id: Set(user_id.0),
+                        role_slug: Set(role_slug.0),
+                        ..Default::default()
+                    };
+
+                    user_role.insert(&self.db).await.unwrap();
+                }
+            }
+            CliCommand::ShowRoles => {
+                let roles = entities::role::Entity::find()
+                    .all(&self.db)
+                    .await
+                    .unwrap();
+
+                for role in roles {
+                    println!("{:?}", role);
+                }
+            }
+            CliCommand::ShowRole(slug) => {
+                let role = entities::role::Entity::find()
+                    .filter(entities::role::Column::Slug.eq(slug.0))
+                    .one(&self.db)
+                    .await
+                    .unwrap();
+
+                println!("{:?}", role);
+            }
+            CliCommand::ShowUser(id) => {
+                let user_select: Vec<UserAndRole> = user::Entity::find_by_id(id.0)
+                    .column(users_roles::Column::RoleSlug)
+                    .join_rev(
+                        JoinType::LeftJoin,
+                        users_roles::Entity::belongs_to(user::Entity)
+                            .from(users_roles::Column::UserId)
+                            .to(user::Column::Id)
+                            .into()
+                    )
+                    .into_model::<UserAndRole>()
+                    .all(&self.db)
+                    .await
+                    .unwrap();
+
+                for user in user_select {
+                    println!("{:?}", user);
+                }
+            }
+            CliCommand::ShowUsers => {
+                let user_select: Vec<UserAndRole> = user::Entity::find()
+                    .column(users_roles::Column::RoleSlug)
+                    .join_rev(
+                        JoinType::LeftJoin,
+                        users_roles::Entity::belongs_to(user::Entity)
+                            .from(users_roles::Column::UserId)
+                            .to(user::Column::Id)
+                            .into()
+                    )
+                    .into_model::<UserAndRole>()
+                    .all(&self.db)
+                    .await
+                    .unwrap();
+
+                for user in user_select {
+                    println!("{:?}", user);
+                }
             }
         }
     }
